0164b91898ebb40299d3fe337c6e88aac3f246df
Leah Pavone <leah@Ls-MacBook-Pro.socal.rr.com>
2023-06-20 15:21:57 -0700

    updates

--- src/Sections/Projects/ProjectData.js
+++ src/Sections/Projects/ProjectData.js
@@ -1,10 +1,14 @@
+import bytb10 from '../../Static/project-media/bytb10-video.mp4';
+import rickAndMorty from '../../Static/project-media/rick-and-morty-video.mp4';
+import lonliestChatroom from '../../Static/project-media/lonliest-chatroom-video.mp4';
+
 export const ProjectData = [
   {
     name: 'Beyond the Big 10',
     madeWith: ['React', 'Material UI', 'Firebase', 'Node.js'],
     description:
       'Responsive website created for Beyond the Big 10 Podcast Network. Utilized React, Material UI, Firebase and Node.js',
-    media: '',
+    media: bytb10,
     liveSite: 'https://beyondthebig10.com',
     githubRepo: '',
     backgroundColor: '#14294b'
@@ -14,7 +18,7 @@ export const ProjectData = [
     madeWith: ['React', 'CSS', 'API'],
     description:
       'Responsive Website that allows users to search for their favorite Rick and Morty characters and get info about them, such as all of the episodes they starred in. Utilizes React, CSS, and Rick and Morty API. ',
-    media: '',
+    media: rickAndMorty,
     liveSite: '',
     githubRepo: 'https://github.com/leahpavone/rick-and-morty_react',
     backgroundColor: '#14294b'
@@ -24,7 +28,7 @@ export const ProjectData = [
     madeWith: ['JavaScript', 'HTML', 'CSS', 'API'],
     description:
       'Website that allows users to write in a chat and get back answers by calling the Chuck Norris API.',
-    media: '',
+    media: lonliestChatroom,
     liveSite: 'https://leahpavone.github.io/TheLonliestChatroom/',
     githubRepo: 'https://github.com/leahpavone/TheLonliestChatroom',
     backgroundColor: '#14294b'
